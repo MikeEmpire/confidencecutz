b7765641b79d6c2c8647b58f49928a106a10319c
Michael Olie <mike@grolens.com>
2023-05-11 08:52:14 -0700

    added styles to mobile view (master)

--- src/components/Footer/index.tsx
+++ src/components/Footer/index.tsx
@@ -6,29 +6,15 @@ import { SiInstagram } from "@react-icons/all-files/si/SiInstagram";
 const Footer = () => {
   return (
     <motion.footer>
-      <section>
-        <article>
-          <header>Confidence Cutz</header>
-          <ul>
-            <a href="https://www.instagram.com/california_kingdoc/">
-              <SiInstagram />
-            </a>
-            <FaFacebook />
-          </ul>
-        </article>
+      <section style={{ flexDirection: "row", gap: "1.4em" }}>
+        <SiInstagram size="24" />
+        <FaFacebook size="24" />
       </section>
       <section>
-        <article>
-          <a href="tel:3238936719">(323)893-6719</a>
-        </article>
-        <article>
-          <a href="https://www.google.com/search?sxsrf=ALiCzsYn0fP51u2OeN5ODAXIzkDyQ2Jd8g:1654324141372&q=Confidence+Cutz&ludocid=3971325457786703188&gsas=1&lsig=AB86z5Wx4kVSssCCjfSJbZkfEx9D&sa=X&ved=2ahUKEwjmtajWlZP4AhVPmeAKHcE0ADsQoAJ6BAgfEAc&biw=1680&bih=939&dpr=2">
-            440 E Manchester Blvd, Inglewood CA 90301
-          </a>
-        </article>
-      </section>
-      <section>
-        <article>Contact Us</article>
+        <header>Find us at</header>
+        <a href="https://www.google.com/search?sxsrf=ALiCzsYn0fP51u2OeN5ODAXIzkDyQ2Jd8g:1654324141372&q=Confidence+Cutz&ludocid=3971325457786703188&gsas=1&lsig=AB86z5Wx4kVSssCCjfSJbZkfEx9D&sa=X&ved=2ahUKEwjmtajWlZP4AhVPmeAKHcE0ADsQoAJ6BAgfEAc&biw=1680&bih=939&dpr=2">
+          440 E Manchester Blvd <br /> Inglewood CA 90301
+        </a>
       </section>
     </motion.footer>
   );
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -32,44 +32,56 @@ const IndexPage = () => {
   return (
     <main>
       <title>Confidence Cutz</title>
-      <motion.div
-        style={{
-          background: "black",
-          position: "absolute",
-          width: "100%",
-          zIndex: 100,
-        }}
-        variants={containerVariants}
-        animate="hidden"
-        initial="appear"
-        className="content__container"
-      >
-        <motion.h4
-          variants={textVariant}
-          transition={{ type: "spring", bounce: 0.5 }}
-        >
-          Confidence Cutz
-        </motion.h4>
-        <h6 className="subtitle">
-          where looking and feeling your best is our top priority
-        </h6>
-        <h6 className="title-caption">
-          Our services don't stop at haircuts, either. We offer a range of
-          grooming services to keep you looking and feeling your best, including
-          beard trims, hot shaves, etc!
-        </h6>
-        <button className="glow-on-hover" id="cta">
-          Book A Cut
-        </button>
-      </motion.div>
-      <div className="video__container">
-        <div id="background-video__mesh" />
-        <video id="background-video" autoPlay loop muted>
-          <source src={mainVideo} type="video/mp4" />
-        </video>
-        <div className="vignette" />
+      <div>
+        <div>
+          <motion.div
+            style={{
+              background: "black",
+              position: "absolute",
+              width: "100%",
+              zIndex: 100,
+            }}
+            variants={containerVariants}
+            animate="hidden"
+            initial="appear"
+            className="content__container"
+          >
+            <motion.h4
+              variants={textVariant}
+              transition={{ type: "spring", bounce: 0.5 }}
+            >
+              Confidence Cutz
+            </motion.h4>
+            <h6 className="subtitle">
+              where looking and feeling your best is our top priority
+            </h6>
+            <h6 className="title-caption">
+              Our services don't stop at haircuts, either. We offer a range of
+              grooming services to keep you looking and feeling your best,
+              including beard trims, hot shaves, etc!
+            </h6>
+            <button className="glow-on-hover" id="cta">
+              Book A Cut
+            </button>
+          </motion.div>
+          <div className="video__container">
+            <div id="background-video__mesh" />
+            <video id="background-video" autoPlay loop muted>
+              <source src={mainVideo} type="video/mp4" />
+            </video>
+            <div className="vignette" />
+          </div>
+          <Footer />
+        </div>
+        <menu>
+          <section>
+            <h6 className="contact-link">Contact Us</h6>
+          </section>
+          <section>
+            <h6 className="menu-link">Book A Cut</h6>
+          </section>
+        </menu>
       </div>
-      <Footer />
     </main>
   );
 };
